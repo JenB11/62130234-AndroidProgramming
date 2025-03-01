6adb8f74c1f932ca26f64074171b91a05f44ec5d
JenB <ntdat.c302@gmail.com>
2025-03-01 14:56:16 +0700

    Update Ex3
    Done

--- Ex3_CalculateApp/app/src/main/java/com/jenb/ntdat/ex3_calculateapp/MainActivity.java
+++ Ex3_CalculateApp/app/src/main/java/com/jenb/ntdat/ex3_calculateapp/MainActivity.java
@@ -39,4 +39,60 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void nutTru(View view){
+        EditText editTextA = findViewById(R.id.edtA);
+        EditText editTextB = findViewById(R.id.edtB);
+        EditText editTextKQ = findViewById(R.id.edtKQ);
+
+        String strA = editTextA.getText().toString();
+        String strB = editTextB.getText().toString();
+
+        int soA = Integer.parseInt(strA);
+        int soB = Integer.parseInt(strB);
+
+        int Hieu = soA - soB;
+
+        String strKQ = String.valueOf(Hieu);
+
+        editTextKQ.setText(strKQ);
+    }
+
+    public void nutNhan(View view){
+        EditText editTextA = findViewById(R.id.edtA);
+        EditText editTextB = findViewById(R.id.edtB);
+        EditText editTextKQ = findViewById(R.id.edtKQ);
+
+        String strA = editTextA.getText().toString();
+        String strB = editTextB.getText().toString();
+
+        int soA = Integer.parseInt(strA);
+        int soB = Integer.parseInt(strB);
+
+        int Tich = soA * soB;
+
+        String strKQ = String.valueOf(Tich);
+
+        editTextKQ.setText(strKQ);
+    }
+
+    public void nutChia(View view){
+        EditText editTextA = findViewById(R.id.edtA);
+        EditText editTextB = findViewById(R.id.edtB);
+        EditText editTextKQ = findViewById(R.id.edtKQ);
+
+        String strA = editTextA.getText().toString();
+        String strB = editTextB.getText().toString();
+
+
+        int soA = Integer.parseInt(strA);
+        int soB = Integer.parseInt(strB);
+
+        int Thuong = soA / soB;
+
+        String strKQ = String.valueOf(Thuong);
+
+        editTextKQ.setText(strKQ);
+    }
+
+
 }

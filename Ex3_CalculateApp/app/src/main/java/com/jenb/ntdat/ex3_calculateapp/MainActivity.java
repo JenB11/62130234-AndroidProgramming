638e391cded048bae26700e58346cbc468dfa6b6
JenB <ntdat.c302@gmail.com>
2025-03-01 13:45:53 +0700

    Update Ex3_CalculateApp

--- Ex3_CalculateApp/app/src/main/java/com/jenb/ntdat/ex3_calculateapp/MainActivity.java
+++ Ex3_CalculateApp/app/src/main/java/com/jenb/ntdat/ex3_calculateapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.jenb.ntdat.ex3_calculateapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,8 +16,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_main);
+    }
+
+    public void nutCong(View view){
+        EditText editTextA = findViewById(R.id.edtA);
+        EditText editTextB = findViewById(R.id.edtB);
+        EditText editTextKQ = findViewById(R.id.edtKQ);
+        
 
     }
+
 }

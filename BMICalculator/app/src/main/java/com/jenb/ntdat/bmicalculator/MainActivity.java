b9d9f0d26932c9a3eba868165e820cf67ea18fc9
JenB <ntdat.c302@gmail.com>
2025-03-02 15:11:16 +0700

    Update layout and function
    thêm thanh trượt và hàm xử lý thanh trượt

--- BMICalculator/app/src/main/java/com/jenb/ntdat/bmicalculator/MainActivity.java
+++ BMICalculator/app/src/main/java/com/jenb/ntdat/bmicalculator/MainActivity.java
@@ -1,6 +1,8 @@
 package com.jenb.ntdat.bmicalculator;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +11,56 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tvKG, tvCM;
+    SeekBar sbWeight, sbHeight;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        Tim_Dieu_khien();
+        init();
+    }
 
+    public void Tim_Dieu_khien(){
+        tvKG = findViewById(R.id.tvKG);
+        sbWeight = findViewById(R.id.sbWeight);
+        tvCM = findViewById(R.id.tvCM);
+        sbHeight = findViewById(R.id.sbHeight);
+    }
+
+    public void init(){
+        sbWeight.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                tvKG.setText(i + "KG");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        sbHeight.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                tvCM.setText(i + "CM");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }

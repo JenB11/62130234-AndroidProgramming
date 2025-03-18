6915c91b34c4ee9121e23018d1f1eb8d66a33095
JenB <ntdat.c302@gmail.com>
2025-03-18 16:07:59 +0700

    Update LoginPage

--- LoginPage/app/src/main/java/com/jenb/ntdat/loginpage/MainActivity.java
+++ LoginPage/app/src/main/java/com/jenb/ntdat/loginpage/MainActivity.java
@@ -1,6 +1,10 @@
 package com.jenb.ntdat.loginpage;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +20,19 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
+        EditText username = findViewById(R.id.username);
+        EditText password = findViewById(R.id.password);
+        Button loginButton = findViewById(R.id.LoginButton);
 
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+                    Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
